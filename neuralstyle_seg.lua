bab66af1c3b8d451a430aba4a5f783199c089de5
Po-Hsien Chu <stegben.benjamin@gmail.com>
2017-08-12 16:07:33 +0800

    unify indentation of neuralstyle_seg.lua

--- neuralstyle_seg.lua
+++ neuralstyle_seg.lua
@@ -285,18 +285,18 @@ function deprocess(img)
 end
 
 function deepcopy(orig)
-    local orig_type = type(orig)
-    local copy
-    if orig_type == 'table' then
-        copy = {}
-        for orig_key, orig_value in next, orig, nil do
-            copy[deepcopy(orig_key)] = deepcopy(orig_value)
-        end
-        setmetatable(copy, deepcopy(getmetatable(orig)))
-    else -- number, string, boolean, etc
-        copy = orig
+  local orig_type = type(orig)
+  local copy
+  if orig_type == 'table' then
+    copy = {}
+    for orig_key, orig_value in next, orig, nil do
+      copy[deepcopy(orig_key)] = deepcopy(orig_value)
     end
-    return copy
+    setmetatable(copy, deepcopy(getmetatable(orig)))
+  else -- number, string, boolean, etc
+    copy = orig
+  end
+  return copy
 end
 
 -- Define an nn Module to compute content loss in-place
